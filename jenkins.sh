b7f2ee3471d4c6d19dc590bdece1642145cea64c
Patrick Zysset <patrick.zysset@netmodule.com>
2014-12-12 16:40:46 +0100

    build - remove mixed tab/spaces
    - replace all tabs with spaces as within other scripts
    
    Signed-off-by: Patrick Zysset <patrick.zysset@netmodule.com>

--- jenkins.sh
+++ jenkins.sh
@@ -4,115 +4,115 @@
 
 removeWorkDir()
 {
-	if [ -d $WORK_DIR ]; then
-		rm -fr $WORK_DIR
-	fi
-	return $?
+  if [ -d $WORK_DIR ]; then
+    rm -fr $WORK_DIR
+  fi
+  return $?
 }
 
 removeImageDir()
 {
-	if [ -d $IMAGE_DIR ]; then
-		rm -fr  $IMAGE_DIR
-	fi
-	return $?
+  if [ -d $IMAGE_DIR ]; then
+    rm -fr  $IMAGE_DIR
+  fi
+  return $?
 }
 
 createImageDir()
 {
-	if [ ! -d $IMAGE_DIR ]; then
-		mkdir $IMAGE_DIR
-	fi
-	return $?
+  if [ ! -d $IMAGE_DIR ]; then
+    mkdir $IMAGE_DIR
+  fi
+  return $?
 }
 
 nightly-linux()
 {
-	$BUILD_SCRIPT build
-	if [ $? -eq 0 ]; then
-		cp $TMP_IMAGE_DIR/$IMAGE_NAME $IMAGE_DIR/$IMAGE_NAME_RAW"_nightly-"$BUILD_NUMBER"."$IMAGE_NAME_EXT
-	else
-		echo "Build failed!"
-		exit -1
-	fi
+  $BUILD_SCRIPT build
+  if [ $? -eq 0 ]; then
+    cp $TMP_IMAGE_DIR/$IMAGE_NAME $IMAGE_DIR/$IMAGE_NAME_RAW"_nightly-"$BUILD_NUMBER"."$IMAGE_NAME_EXT
+  else
+    echo "Build failed!"
+    exit -1
+  fi
 }
 
 nightly-uboot()
 {
-	$BUILD_SCRIPT build-u-boot-zx3
-	if [ $? -eq 0 ]; then
-		cp $TMP_IMAGE_DIR/u-boot.elf $IMAGE_DIR/"u-boot_ze7000_nightly-"$BUILD_NUMBER".elf"
-	else
-		echo "Build failed!"
-		exit -1
-	fi
+  $BUILD_SCRIPT build-u-boot-zx3
+  if [ $? -eq 0 ]; then
+    cp $TMP_IMAGE_DIR/u-boot.elf $IMAGE_DIR/"u-boot_ze7000_nightly-"$BUILD_NUMBER".elf"
+  else
+    echo "Build failed!"
+    exit -1
+  fi
 }
 
 nightly()
 {
-	removeWorkDir
-	removeImageDir
-	createImageDir
-	$BUILD_SCRIPT init nightly
-	if [ $? -eq 0 ]; then
+  removeWorkDir
+  removeImageDir
+  createImageDir
+  $BUILD_SCRIPT init nightly
+  if [ $? -eq 0 ]; then
     nightly-uboot
     nightly-linux
-	else
-		echo "Init failed"
-		exit -1
-	fi
+  else
+    echo "Init failed"
+    exit -1
+  fi
 }
 
 release()
 {
-	removeWorkDir
-	removeImageDir
-	createImageDir
-	$BUILD_SCRIPT init release
-	if [ $? -eq 0 ]; then
-		$BUILD_SCRIPT build
-		if [ $? -eq 0 ]; then
-			cp $TMP_IMAGE_DIR/$IMAGE_NAME $IMAGE_DIR/$IMAGE_NAME_RAW"_release-"$(date +%Y.%m)"-"$BUILD_NUMBER"."$IMAGE_NAME_EXT
-		else
-			echo "Build failed!"
-			exit -1
-		fi
-	else
-		echo "Init failed"
-		exit -1
-	fi
+  removeWorkDir
+  removeImageDir
+  createImageDir
+  $BUILD_SCRIPT init release
+  if [ $? -eq 0 ]; then
+    $BUILD_SCRIPT build
+    if [ $? -eq 0 ]; then
+      cp $TMP_IMAGE_DIR/$IMAGE_NAME $IMAGE_DIR/$IMAGE_NAME_RAW"_release-"$(date +%Y.%m)"-"$BUILD_NUMBER"."$IMAGE_NAME_EXT
+    else
+      echo "Build failed!"
+      exit -1
+    fi
+  else
+    echo "Init failed"
+    exit -1
+  fi
 }
 
 release-uboot()
 {
-	removeWorkDir
-        removeImageDir
-	createImageDir
-	$BUILD_SCRIPT init release
-	if [ $? -eq 0 ]; then
-		$BUILD_SCRIPT build-u-boot-zx3
-		if [ $? -eq 0 ]; then
-			cp $TMP_IMAGE_DIR/u-boot.elf $IMAGE_DIR/"u-boot_ze7000_release-"$(date +%Y.%m)"-"$BUILD_NUMBER".elf"
-		else
-			echo "Build failed!"
-			exit -1
-		fi
-	else
-		echo "Init failed"
-		exit -1
-	fi
+  removeWorkDir
+  removeImageDir
+  createImageDir
+  $BUILD_SCRIPT init release
+  if [ $? -eq 0 ]; then
+    $BUILD_SCRIPT build-u-boot-zx3
+    if [ $? -eq 0 ]; then
+      cp $TMP_IMAGE_DIR/u-boot.elf $IMAGE_DIR/"u-boot_ze7000_release-"$(date +%Y.%m)"-"$BUILD_NUMBER".elf"
+    else
+      echo "Build failed!"
+      exit -1
+    fi
+  else
+    echo "Init failed"
+    exit -1
+  fi
 }
 
 case $1 in
 nightly)
-	nightly
-	;;
+  nightly
+  ;;
 
 release)
-	release
-	;;
+  release
+  ;;
 
 release-uboot)
-	release-uboot
-	;;
+  release-uboot
+  ;;
 esac
