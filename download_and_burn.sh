a26d7c3fc9e6ce61667fe4e30ddb4ef2b8b07c7a
David Andrey <david.andrey@netmodule.com>
2015-06-23 13:23:42 +0200

    Revert "added sdburn stations script"
    This reverts commit 4a06b6144589b21b18554cfa5987ba036af853c7.

--- download_and_burn.sh
+++
@@ -1,3 +0,0 @@
-wget -N http://jenkins.netmodule.intranet/view/ZE7000/job/ze7000-release/lastSuccessfulBuild/artifact/images/ze7000-image-ze7000-zynq7.tar.bz2
-wget -N http://jenkins.netmodule.intranet/view/ZE7000/job/ze7000-boot-release/lastSuccessfulBuild/artifact/ready_to_test/ze7000_boot.bin 
-sudo ./install-sd.sh /dev/sdd ze7000_boot.bin ze7000-image-ze7000-zynq7.tar.bz2
